1546d77e8581283008c53115b4c7a5d927a53d2d
MohamedHassanYoussef <mohamedhassan199489@gmail.com>
2024-10-22 11:52:57 +0300

    Modified: UpdateProduct.kt

--- app/src/main/java/com/example/commerceapp/model/UpdateProduct.kt
+++ app/src/main/java/com/example/commerceapp/model/UpdateProduct.kt
@@ -12,11 +12,17 @@ data class Product(
     val vendor: String,
     @SerializedName("product_type") val productType: String,
     val variants: List<Variant22>,
+    val images: List<ImageEdit?>,
 
 )
 
 data class Variant22(
     val price: String,
     @SerializedName("option_2") val option2: String? = null
+)
+
+data class ImageEdit (
+    val src: String?,
+    val alt: String?
 )
 
