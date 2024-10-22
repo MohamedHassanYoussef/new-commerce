6d22f545eeeea15b8cfdeb326f65044793bfa5a8
MohamedHassanYoussef <mohamedhassan199489@gmail.com>
2024-10-22 10:03:14 +0300

    Added LoginActivity.kt

--- app/src/main/java/com/example/commerceapp/login/LoginActivity.kt
+++ app/src/main/java/com/example/commerceapp/login/LoginActivity.kt
@@ -1,20 +1,45 @@
-package com.example.commerceapp
+package com.example.commerceapp.login
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.commerceapp.MainActivity
+import com.example.commerceapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            val userName = binding.editTextTextUserName.text.toString().trim()
+            val password = binding.editTextTextPassword.text.toString().trim()
+
+
+            val adminName = "Mohamed"
+            val adminPassword = "Mohamed@123"
+
+            if (userName.isBlank()) {
+                Toast.makeText(this, "Please enter your username", Toast.LENGTH_SHORT).show()
+            } else if (password.isBlank()) {
+                Toast.makeText(this, "Please enter your password", Toast.LENGTH_SHORT).show()
+            } else if (userName == adminName && password == adminPassword) {
+                Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+                val Intent: Intent = Intent(
+                    this,
+                    MainActivity::class.java
+                )
+                startActivity(Intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
